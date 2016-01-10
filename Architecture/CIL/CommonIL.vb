1f2d8b8ee052ac62563a3e5c0eb8d8c2fe6ab20b
zenuas@gmail.com <zenuas@gmail.com>
2016-01-10 18:30:03 +0900

    CILの作成

---
+++ Architecture/CIL/CommonIL.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Reflection
+Imports System.Reflection.Emit
+Imports Roku.Manager
+
+
+Namespace Architecture.CIL
+
+    <ArchitectureName("CIL")>
+    Public Class CommonIL
+        Implements IArchitecture
+
+        Public Overridable Property Root As RkNamespace
+        Public Overridable Property EntryPoint As String = "Global"
+        Public Overridable Property Subsystem As PEFileKinds = PEFileKinds.ConsoleApplication
+        Public Overridable Property Assembly As AssemblyBuilder
+        Public Overridable Property [Module] As ModuleBuilder
+
+        Public Overridable Sub Assemble(ns As RkNamespace) Implements IArchitecture.Assemble
+
+            Me.Root = ns
+
+            Dim name As New AssemblyName(Me.EntryPoint)
+            Me.Assembly = System.AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.Save)
+            Me.Module = Me.Assembly.DefineDynamicModule(Me.EntryPoint, System.IO.Path.GetRandomFileName, False)
+        End Sub
+
+        Public Overridable Sub Optimize() Implements IArchitecture.Optimize
+
+            'Throw New NotImplementedException()
+        End Sub
+
+        Public Overridable Sub Emit(path As String) Implements IArchitecture.Emit
+
+            If Me.Subsystem <> PEFileKinds.Dll Then
+
+                ' global sub main() {EntryPoint.new();}
+                Dim method = Me.Module.DefineGlobalMethod("__EntryPoint", MethodAttributes.Static Or MethodAttributes.Family, GetType(System.Void), System.Type.EmptyTypes)
+
+                Dim il = method.GetILGenerator
+                'il.Emit(OpCodes.Newobj, Me.Module.GetType(Me.EntryPoint).GetConstructor(System.Type.EmptyTypes))
+                'il.Emit(OpCodes.Pop)
+                il.Emit(OpCodes.Ret)
+
+                Me.Assembly.SetEntryPoint(method, Me.Subsystem)
+            End If
+
+            Me.Module.CreateGlobalFunctions()
+            Dim temp = System.IO.Path.GetFileName(Me.Module.FullyQualifiedName)
+            Try
+                Me.Assembly.Save(temp)
+                System.IO.File.Copy(temp, path, True)
+
+            Finally
+                System.IO.File.Delete(temp)
+
+            End Try
+        End Sub
+    End Class
+
+End Namespace
---
+++ Roku.ildecomp.bat
@@ -0,0 +1,50 @@
+@prompt $$$S
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+set PATH=%PATH%;%PROGRAMFILES(X86)%\MSBuild\14.0\Bin;%PROGRAMFILES(X86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools
+set  BAT=Roku.release.bat ildecomp.bat
+set WORK=bin\netf4
+set  OUT=%WORK%\roku.exe
+set   RK=tests/test.rk
+
+call :filelist SRCS *.vb
+
+call :make Compiler\MyParser.vb roku.y %BAT% || call :echoexec call yanp.bat --quit
+call :make %OUT% %SRCS%                %BAT% || call :echoexec call Roku.release.bat --quit
+call :make a.exe %OUT% %RK%            %BAT% || call :echoexec %OUT% %RK% -o a.exe -a CIL
+call :make a.il  a.exe                 %BAT% || call :echoexec ildasm a.exe /out:a.il /nobar
+
+call :echoexec .\a.exe
+
+pause
+exit /b 0
+
+
+:echoexec
+	echo $ %*
+	%*
+	echo.
+	exit /b %ERRORLEVEL%
+
+:make
+	REM echo WScript.Echo CreateObject("Scripting.FileSystemObject").GetFile(WScript.Arguments(0)).DateLastModified > @cscript_oneliner.vbs
+	REM call :setcmd target "cscript @cscript_oneliner.vbs //nologo %~s1"
+	cscript //nologo ..\legacy\build-tools\make.vbs %*
+	exit /b %ERRORLEVEL%
+
+:filelist
+	set %1=
+	for /r %%a in (%2) do set %1=!%1! "%%a"
+	exit /b 0
+
+:setcmd
+	set _P=
+	for /f "delims=" %%v IN ('%2') do (
+		set _P=!_P!%%v^
+
+
+	)
+	set %1=!_P!
+	exit /b 0
+
